4141025d69e58b2551818f37f71329f360d75057
antoinedelia <antoine.delia@gmail.com>
2022-01-29 12:54:30 +0100

    Add text and gradient to cover

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import time
 from spotify import Spotify
 from dotenv import load_dotenv
 from loguru import logger
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import requests
 
 load_dotenv()
@@ -16,6 +16,7 @@ SPOTIFY_PLAYLISTS_URIS = [
     "https://open.spotify.com/playlist/0JP3smzah2mTnxIZIVjVX0?si=e33aaca9d1334dc6"
 ]
 ARTIST_IMAGE_SIZE = 640
+IMAGE_SIZE = ARTIST_IMAGE_SIZE * 2
 scopes = "user-library-read playlist-modify-public playlist-modify-private playlist-read-private ugc-image-upload"
 
 
@@ -40,7 +41,7 @@ def main():
                     }
                 )
 
-        playlist_cover = Image.new("RGB", (ARTIST_IMAGE_SIZE * 2, ARTIST_IMAGE_SIZE * 2))
+        playlist_cover = Image.new("RGB", (IMAGE_SIZE, IMAGE_SIZE))
 
         counter = Counter([artist["name"] for artist in artists])
         four_most_common = counter.most_common(4)
@@ -56,6 +57,28 @@ def main():
             y = index % 2 * ARTIST_IMAGE_SIZE
             w, h = img.size
             playlist_cover.paste(img, (x, y, x + w, y + h))
+
+        # Converting back to RGBA to apply gradient
+        playlist_cover = playlist_cover.convert('RGBA')
+        gradient = Image.new('L', (1, IMAGE_SIZE), color=0xFF)
+        current_color = 255
+        for y in range(IMAGE_SIZE):
+            if y % 2 == 0:
+                current_color -= 1
+            current_y = IMAGE_SIZE - y - 1
+            gradient.putpixel((0, current_y), current_color)
+        alpha = gradient.resize(playlist_cover.size)
+        black_im = Image.new('RGBA', (IMAGE_SIZE, IMAGE_SIZE), color=0)  # i.e. black
+        black_im.putalpha(alpha)
+        playlist_cover = Image.alpha_composite(playlist_cover, black_im)
+
+        # Adding the text
+        draw = ImageDraw.Draw(playlist_cover)
+        font = ImageFont.truetype('./fonts/Roboto-Bold.ttf', 100)
+        draw.text(xy=(ARTIST_IMAGE_SIZE, IMAGE_SIZE - 50), text=playlist_name, fill=(255, 255, 255), anchor="ms", font=font, align="center")
+
+        # Converting back to RGB
+        playlist_cover = playlist_cover.convert('RGB')
 
         logger.info("Saving image...")
         playlist_cover.save(os.path.expanduser(f"{playlist_name}.jpg"))
